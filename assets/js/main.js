ef0944951cc087019de4df5b3b55e91243119658
Alberth <orlando.alb77@gmail.com>
2022-08-21 21:53:05 -0400

    traducciones y seccion neraverse

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+$.getJSON("lang/lang.json", function (json) {
+  // Preguntamos en la memoria local si hay un lenguaje guardado
+  if (!localStorage.getItem("lang")) {
+    localStorage.setItem("lang", "en");
+  }
+  let def = localStorage.getItem("lang");
+  $(".lang").each(function (index, value) {
+    $(this).html(json[def][$(this).attr("key")]);
+  });
+  $(".translate").click(function () {
+    let lang = $(this).attr("id");
+    localStorage.setItem("lang", lang);
+    $(".lang").each(function (index, value) {
+      $(this).html(json[lang][$(this).attr("key")]);
+    });
+  });
+});
 $(function () {
   var wind = $(window);
 
